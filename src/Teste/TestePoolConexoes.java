5462f60895fa22368d67e4b117a60a4027c74a3f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-15 11:30:07 -0300

    update

--- src/Teste/TestePoolConexoes.java
+++ src/Teste/TestePoolConexoes.java
@@ -1,5 +1,20 @@
 package Teste;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.fiap.connection.ConnectionFactory;
+
 public class TestePoolConexoes {
 
+	public static void main(String[] args) throws SQLException{
+		for (int i = 0; i < 50; i++) {
+			Connection conexao = ConnectionFactory.conectar();
+			System.out.println("Conexao: " + i);
+		}
+	
+	}
+	
+	
+	
 }
